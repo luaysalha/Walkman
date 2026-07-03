3ec63abda199cd8661c75fba1bc03bcf636e6251
luaysalha <luaysalha@gmail.com>
2026-07-04 00:06:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='walkman-v4';
+const CACHE='walkman-v6';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])));
   self.skipWaiting();
